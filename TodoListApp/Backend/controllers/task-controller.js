9fd86fead5effca002bf42ce43c9993e72155a93
william <tooknown27@gmail.com>
2025-03-07 03:54:03 +0000

    started the front end

---
+++ TodoListApp/Backend/controllers/task-controller.js
@@ -0,0 +1,7 @@
+//get all tasks
+
+//add a task 
+
+//edit a task 
+
+//delete a task
---
+++ TodoListApp/Backend/routes/tasks-route.js
@@ -0,0 +1,9 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+router.get('/tasks',);
+router.post('/add',);
+router.put('/edit/:id');
+router.delete('/delete/:id',); 
+
+module.exports = router;
